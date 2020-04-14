fc86d4893bc1bed44d0beb68e77e5e2a51035099
ReeceASharp <ReeceA.Sharp@gmail.com>
2020-04-13 23:17:16 -0600

    initial commit

---
+++ src/main/java/cs455/hadoop/q_one/WordCountMapper.java
@@ -0,0 +1,36 @@
+package cs455.hadoop.q_one;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+/**
+ * Mapper: Reads line by line, split them into words. Emit <word, 1> pairs.
+ */
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        //get string, and convert to array, we know the elements that we care about
+        String[] vals = value.toString().replace("\"", "").split(",");
+
+        String key_ = vals[21] + ',' + vals[1] + ',' + vals[2];
+
+        //element 21 is the state string name
+        context.write(new Text(key_), new IntWritable(1));
+        
+        // tokenize into words.
+        //StringTokenizer itr = new StringTokenizer(value.toString());
+        // emit word, count pairs.
+
+
+
+        //while (itr.hasMoreTokens()) {
+        //    context.write(new Text(itr.nextToken()), new IntWritable(1));
+        // }
+    }
+}
